f510fd764d663b14b069d98bbf714f89eafd34db
SunWoong <sunwoong1122@gmail.com>
2022-10-04 23:21:15 +0900

    feat: 로그인 기능 구현

--- app/src/main/java/org/sopt/sample/presentation/signin/view/SignInActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/signin/view/SignInActivity.kt
@@ -1,16 +1,83 @@
 package org.sopt.sample.presentation.signin.view
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.data.model.User
 import org.sopt.sample.databinding.ActivitySignInBinding
+import org.sopt.sample.presentation.common.EventObserve
+import org.sopt.sample.presentation.common.ViewModelFactory
+import org.sopt.sample.presentation.home.view.HomeActivity
+import org.sopt.sample.presentation.signin.viewmodel.SignInViewModel
+import org.sopt.sample.presentation.signup.view.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
+    private val viewModel: SignInViewModel by viewModels { ViewModelFactory() }
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.lifecycleOwner = this
+
+        setOnClickListener()
+        setObservers()
+        setActivityResultLauncher()
+    }
+
+    private fun setOnClickListener() {
+        with(binding) {
+            signInLoginBtn.setOnClickListener {
+                viewModel.signIn(signInIdEt.text.toString(), signInPasswordEt.text.toString())
+            }
+            signInSignUpBtn.setOnClickListener {
+                startSignUpActivity()
+            }
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.signInEvent.observe(
+            this, EventObserve { isPossible ->
+                if (isPossible) {
+                    Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                    startHomeActivity()
+                } else {
+                    Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    private fun setActivityResultLauncher() {
+        launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val user: User? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        result.data?.getSerializableExtra("user", User::class.java)
+                    } else {
+                        result.data?.getSerializableExtra("user") as User?
+                    }
+                    viewModel.setUser(user!!)
+                }
+            }
+    }
+
+    private fun startSignUpActivity() {
+        launcher.launch(Intent(this, SignUpActivity::class.java))
+    }
+
+    private fun startHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 }
