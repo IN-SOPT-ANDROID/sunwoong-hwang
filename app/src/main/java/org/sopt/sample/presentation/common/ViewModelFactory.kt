139cefa4f223db868fd5230039492513fdfb16a2
SunWoong <sunwoong1122@gmail.com>
2022-10-04 23:20:53 +0900

    feat: view model factory 구현

---
+++ app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
@@ -0,0 +1,23 @@
+package org.sopt.sample.presentation.common
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.sopt.sample.presentation.signin.viewmodel.SignInViewModel
+import org.sopt.sample.presentation.signup.viewmodel.SignUpViewModel
+
+class ViewModelFactory : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(SignInViewModel::class.java) -> {
+                SignInViewModel() as T
+            }
+            modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
+                SignUpViewModel() as T
+            }
+            else -> {
+                throw java.lang.IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
+            }
+        }
+    }
+}
