b08e5ca2f1f69c551a6fd3257f52314913c8c0d5
SunWoong <sunwoong1122@gmail.com>
2022-10-04 23:21:28 +0900

    feat: 회원가입 기능 구현

--- app/src/main/java/org/sopt/sample/presentation/signup/view/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/signup/view/SignUpActivity.kt
@@ -1,17 +1,75 @@
 package org.sopt.sample.presentation.signup.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.data.model.User
 import org.sopt.sample.databinding.ActivitySignUpBinding
+import org.sopt.sample.presentation.common.EventObserve
+import org.sopt.sample.presentation.common.ViewModelFactory
+import org.sopt.sample.presentation.signin.view.SignInActivity
+import org.sopt.sample.presentation.signup.viewmodel.SignUpViewModel
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+    private val viewModel: SignUpViewModel by viewModels { ViewModelFactory() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.lifecycleOwner = this
+
+        setOnClickListener()
+        setObservers()
+        setNavigation()
+    }
+
+    private fun getUser(): User {
+        with(binding) {
+            val id = signUpIdEt.text.toString()
+            val password = signUpPasswordEt.text.toString()
+            val mbti = signUpMbtiEt.text.toString()
+            val part = signUpPartEt.text.toString()
+            val nickname = signUpNicknameEt.text.toString()
+            return User(id, password, mbti, part, nickname)
+        }
+    }
+
+    private fun setOnClickListener() {
+        binding.signUpLoginBtn.setOnClickListener {
+            viewModel.signUp(getUser())
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.signUpEvent.observe(
+            this, EventObserve { isPossible ->
+                if (isPossible) {
+                    Toast.makeText(this, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                    startSignInActivity(viewModel.getUser()!!)
+                } else {
+                    Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    private fun setNavigation() {
+        binding.signUpTb.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    private fun startSignInActivity(user: User) {
+        val intent = Intent(this, SignInActivity::class.java)
+        intent.putExtra("user", user)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
