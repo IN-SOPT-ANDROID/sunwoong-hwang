bff7db274280a99e1fc793d9f753fee6c4bc8999
SunWoong <sunwoong1122@gmail.com>
2022-10-07 11:51:14 +0900

    fix: 회원 상세 정보 -> 회원 data class 통합

--- app/src/main/java/org/sopt/sample/data/model/User.kt
+++ app/src/main/java/org/sopt/sample/data/model/User.kt
@@ -7,5 +7,6 @@ data class User(
     val password: String,
     val mbti: String,
     val part: String,
-    val nickname: String
+    val nickname: String,
+    val profileUrl: String = "https://user-images.githubusercontent.com/81796317/193957207-929b445f-68f7-4ee8-8d2a-2fbd15660dde.jpeg",
 ): Serializable
