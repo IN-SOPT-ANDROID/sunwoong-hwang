d80e291409bfb2f58d06b151eebb9ac12267a2b9
SunWoong <sunwoong1122@gmail.com>
2022-10-12 03:07:03 +0900

    fix: circle image -> default image 변경

--- app/src/main/java/org/sopt/sample/presentation/common/BindingAdapter.kt
+++ app/src/main/java/org/sopt/sample/presentation/common/BindingAdapter.kt
@@ -3,12 +3,11 @@ package org.sopt.sample.presentation.common
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("circleImageUrl")
-fun circleImageUrl(view: ImageView, imageUrl: String?) {
+@BindingAdapter("imageUrl")
+fun imageUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         GlideApp.with(view)
             .load(imageUrl)
-            .circleCrop()
             .into(view)
     }
 }
