3ec8cf6c0d2fec3f099d0b7bf9abea8ab2a1162b
SunWoong <sunwoong1122@gmail.com>
2022-11-10 03:44:29 +0900

    feat: 회원가입 기능 구현 -> 조건 만족 시 회원가입 버튼 활성화 기능 추가 & API 통신 적용

--- app/src/main/java/org/sopt/sample/presentation/signup/view/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/signup/view/SignUpActivity.kt
@@ -1,21 +1,25 @@
 package org.sopt.sample.presentation.signup.view
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import org.sopt.sample.R
-import org.sopt.sample.data.model.User
+import org.sopt.sample.data.model.SignUpRequest
 import org.sopt.sample.databinding.ActivitySignUpBinding
-import org.sopt.sample.presentation.common.EventObserver
-import org.sopt.sample.presentation.common.USER
 import org.sopt.sample.presentation.common.ViewModelFactory
 import org.sopt.sample.presentation.signin.view.SignInActivity
 import org.sopt.sample.presentation.signup.viewmodel.SignUpViewModel
+import org.sopt.sample.util.EMAIL
+import org.sopt.sample.util.EventObserver
+import org.sopt.sample.util.NAME
+import org.sopt.sample.util.PASSWORD
 
 class SignUpActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivitySignUpBinding
     private val viewModel: SignUpViewModel by viewModels { ViewModelFactory(this) }
 
@@ -27,20 +31,55 @@ class SignUpActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
 
-        setOnClickListener()
         setObservers()
+        setOnClickListener()
+        setAddTextChangedListener()
         setNavigation()
     }
 
-    private fun getUser(): User {
+    private fun getUser(): SignUpRequest {
         with(binding) {
-            val id = signUpIdEt.text.toString()
+            val email = signUpIdEt.text.toString()
             val password = signUpPasswordEt.text.toString()
-            val mbti = signUpMbtiEt.text.toString()
-            val part = signUpPartEt.text.toString()
-            val nickname = signUpNicknameEt.text.toString()
-            return User(id, password, mbti, part, nickname)
+            val name = signUpNameEt.text.toString()
+            return SignUpRequest(email, password, name)
         }
+    }
+
+    private fun setObservers() {
+        viewModel.signUpEvent.observe(
+            this, EventObserver { isSuccess ->
+                if (isSuccess) {
+                    Toast.makeText(this, R.string.success_sign_up, Toast.LENGTH_SHORT).show()
+                    startSignInActivity()
+                } else {
+                    Toast.makeText(this, R.string.failure_sign_up, Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+        viewModel.isPromising.observe(
+            this, EventObserver { isSuccess ->
+                if (isSuccess) {
+                    binding.signUpSignUpBtn.isEnabled = true
+                    binding.signUpSignUpBtn.backgroundTintList =
+                        ColorStateList.valueOf(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.black
+                            )
+                        )
+                } else {
+                    binding.signUpSignUpBtn.isEnabled = false
+                    binding.signUpSignUpBtn.backgroundTintList =
+                        ColorStateList.valueOf(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.gray
+                            )
+                        )
+                }
+            }
+        )
     }
 
     private fun setOnClickListener() {
@@ -49,16 +88,30 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun setObservers() {
-        viewModel.signUpEvent.observe(
-            this, EventObserver { isPossible ->
-                if (isPossible) {
-                    startSignInActivity(viewModel.getUser()!!)
+    private fun setAddTextChangedListener() {
+        with(binding) {
+            signUpIdEt.addTextChangedListener {
+                if (signUpIdEt.text.isNullOrEmpty()) {
+                    viewModel.setUserStatus(EMAIL, false)
                 } else {
-                    Toast.makeText(this, R.string.failure_sign_up, Toast.LENGTH_SHORT).show()
+                    viewModel.setUserStatus(EMAIL, true)
                 }
             }
-        )
+            signUpPasswordEt.addTextChangedListener {
+                if (signUpPasswordEt.text.isNullOrEmpty()) {
+                    viewModel.setUserStatus(PASSWORD, false)
+                } else {
+                    viewModel.setUserStatus(PASSWORD, true)
+                }
+            }
+            signUpNameEt.addTextChangedListener {
+                if (signUpNameEt.text.isNullOrEmpty()) {
+                    viewModel.setUserStatus(NAME, false)
+                } else {
+                    viewModel.setUserStatus(NAME, true)
+                }
+            }
+        }
     }
 
     private fun setNavigation() {
@@ -67,10 +120,8 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun startSignInActivity(user: User) {
-        val intent = Intent(this, SignInActivity::class.java)
-        intent.putExtra(USER, user)
-        setResult(RESULT_OK, intent)
+    private fun startSignInActivity() {
+        startActivity(Intent(this, SignInActivity::class.java))
         finish()
     }
 }
