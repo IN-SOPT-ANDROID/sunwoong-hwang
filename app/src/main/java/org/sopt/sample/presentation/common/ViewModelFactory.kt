aed5c74684ba790ebe756d99267c652469184f43
SunWoong <sunwoong1122@gmail.com>
2022-11-10 03:46:33 +0900

    feat: 로그인 & 회원가입 view model 의존성 주입

--- app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
+++ app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
@@ -3,7 +3,11 @@ package org.sopt.sample.presentation.common
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import org.sopt.sample.data.api.ApiClient
+import org.sopt.sample.data.api.AuthService
+import org.sopt.sample.data.repository.AuthRepository
 import org.sopt.sample.data.repository.HomeRepository
+import org.sopt.sample.data.source.AuthDataSourceImpl
 import org.sopt.sample.data.source.HomeDataSourceImpl
 import org.sopt.sample.presentation.home.viewmodel.HomeViewModel
 import org.sopt.sample.presentation.signin.viewmodel.SignInViewModel
@@ -11,14 +15,25 @@ import org.sopt.sample.presentation.signup.viewmodel.SignUpViewModel
 import org.sopt.sample.util.AssetsLoader
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
-
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(SignInViewModel::class.java) -> {
-                SignInViewModel() as T
+                SignInViewModel(
+                    AuthRepository(
+                        AuthDataSourceImpl(
+                            ApiClient.getInstance()!!.create(AuthService::class.java)
+                        )
+                    )
+                ) as T
             }
             modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
-                SignUpViewModel() as T
+                SignUpViewModel(
+                    AuthRepository(
+                        AuthDataSourceImpl(
+                            ApiClient.getInstance()!!.create(AuthService::class.java)
+                        )
+                    )
+                ) as T
             }
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
                 HomeViewModel(HomeRepository(HomeDataSourceImpl(AssetsLoader(context)))) as T
