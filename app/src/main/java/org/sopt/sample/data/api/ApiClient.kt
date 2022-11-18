1e36891da7247ed205390296e419dc47993a46ed
SunWoong <sunwoong1122@gmail.com>
2022-11-18 21:13:32 +0900

    feat: reqres retrofit instance 추가 & getter 구현

--- app/src/main/java/org/sopt/sample/data/api/ApiClient.kt
+++ app/src/main/java/org/sopt/sample/data/api/ApiClient.kt
@@ -9,24 +9,44 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object ApiClient {
-    private const val BASE_URL = "http://3.39.169.52:3000/"
-    private var retrofit: Retrofit? = null
+    private const val SOPT_BASE_URL = "http://3.39.169.52:3000/"
+    private const val REGRES_BASE_URL = "https://reqres.in/"
+    private var soptRetrofit: Retrofit? = null
+    private var regresRetrofit: Retrofit? = null
 
     @OptIn(ExperimentalSerializationApi::class)
-    fun getInstance(): Retrofit? {
-        if (retrofit == null) {
+    fun getRetrofitForSopt(): Retrofit {
+        if (soptRetrofit == null) {
             val logger = HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BASIC
             }
             val client = OkHttpClient.Builder()
                 .addInterceptor(logger)
                 .build()
-            retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
+            soptRetrofit = Retrofit.Builder()
+                .baseUrl(SOPT_BASE_URL)
                 .client(client)
                 .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
                 .build()
         }
-        return retrofit
+        return soptRetrofit!!
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getRetrofitForRegres(): Retrofit {
+        if (regresRetrofit == null) {
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            regresRetrofit = Retrofit.Builder()
+                .baseUrl(REGRES_BASE_URL)
+                .client(client)
+                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+                .build()
+        }
+        return regresRetrofit!!
     }
 }
