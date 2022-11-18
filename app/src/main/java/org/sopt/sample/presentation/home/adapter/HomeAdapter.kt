e3211b93fa4977a5f97af0b5b8ffe3f31f3f0c77
SunWoong <sunwoong1122@gmail.com>
2022-11-18 21:18:30 +0900

    feat: regres 프로필 조회 기능 구현

--- app/src/main/java/org/sopt/sample/presentation/home/adapter/HomeAdapter.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/adapter/HomeAdapter.kt
@@ -2,66 +2,41 @@ package org.sopt.sample.presentation.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import org.sopt.sample.data.model.GithubInformation
-import org.sopt.sample.data.model.User
-import org.sopt.sample.data.model.UserInformation
-import org.sopt.sample.databinding.ItemGithubBinding
+import org.sopt.sample.data.model.Profile
 import org.sopt.sample.databinding.ItemProfileBinding
 
-class HomeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class HomeAdapter : ListAdapter<Profile, HomeAdapter.ProfileViewHolder>(itemDiffCallBack) {
+    private lateinit var inflater: LayoutInflater
 
-    private val userInformations = mutableListOf<UserInformation>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        return when (viewType) {
-            VIEW_TYPE_PROFILE -> HomeUserViewHolder(ItemProfileBinding.inflate(inflater, parent, false))
-            else ->  HomeGithubInformationViewHolder(ItemGithubBinding.inflate(inflater, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+        if (!::inflater.isInitialized) {
+            inflater = LayoutInflater.from(parent.context)
         }
+        return ProfileViewHolder(ItemProfileBinding.inflate(inflater, parent, false))
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is HomeUserViewHolder -> holder.bind(userInformations[position] as User)
-            is HomeGithubInformationViewHolder -> holder.bind(userInformations[position] as GithubInformation)
-        }
+    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+        holder.bind(currentList[position])
     }
 
-    override fun getItemCount(): Int = userInformations.size
-
-    override fun getItemViewType(position: Int): Int {
-        return when (userInformations[position]) {
-            is User -> VIEW_TYPE_PROFILE
-            else -> VIEW_TYPE_GITHUB
-        }
-    }
-
-    fun submitUserInformations(userInformations: List<UserInformation>) {
-        this.userInformations.addAll(userInformations)
-        notifyItemRangeInserted(itemCount, userInformations.size)
-    }
-
-    class HomeUserViewHolder(private val binding: ItemProfileBinding) :
+    class ProfileViewHolder(private val binding: ItemProfileBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(user: User) {
-            binding.user = user
-            binding.executePendingBindings()
-        }
-    }
-
-    class HomeGithubInformationViewHolder(private val binding: ItemGithubBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(githubInformation: GithubInformation) {
-            binding.githubInformation = githubInformation
+        fun bind(profile: Profile) {
+            binding.profile = profile
             binding.executePendingBindings()
         }
     }
 
     companion object {
-        private const val VIEW_TYPE_PROFILE = 0
-        private const val VIEW_TYPE_GITHUB = 1
+        private val itemDiffCallBack = object : DiffUtil.ItemCallback<Profile>() {
+            override fun areItemsTheSame(oldItem: Profile, newItem: Profile): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Profile, newItem: Profile): Boolean =
+                oldItem == newItem
+        }
     }
 }
