a42842d1cfb451d789cef38d4023954f772648bd
SunWoong <sunwoong1122@gmail.com>
2022-11-27 21:32:37 +0900

    refactor: retrofit function -> lazy 생성 변경

--- app/src/main/java/org/sopt/sample/data/api/ApiClient.kt
+++ app/src/main/java/org/sopt/sample/data/api/ApiClient.kt
@@ -11,42 +11,30 @@ import retrofit2.Retrofit
 object ApiClient {
     private const val SOPT_BASE_URL = "http://3.39.169.52:3000/"
     private const val REGRES_BASE_URL = "https://reqres.in/"
-    private var soptRetrofit: Retrofit? = null
-    private var regresRetrofit: Retrofit? = null
 
-    @OptIn(ExperimentalSerializationApi::class)
-    fun getRetrofitForSopt(): Retrofit {
-        if (soptRetrofit == null) {
-            val logger = HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BASIC
-            }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-            soptRetrofit = Retrofit.Builder()
-                .baseUrl(SOPT_BASE_URL)
-                .client(client)
-                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-                .build()
-        }
-        return soptRetrofit!!
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
     }
 
     @OptIn(ExperimentalSerializationApi::class)
-    fun getRetrofitForRegres(): Retrofit {
-        if (regresRetrofit == null) {
-            val logger = HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BASIC
-            }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-            regresRetrofit = Retrofit.Builder()
-                .baseUrl(REGRES_BASE_URL)
-                .client(client)
-                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-                .build()
-        }
-        return regresRetrofit!!
+    val soptRetrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(SOPT_BASE_URL)
+            .client(client)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    val regresRetrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(REGRES_BASE_URL)
+            .client(client)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
     }
 }
