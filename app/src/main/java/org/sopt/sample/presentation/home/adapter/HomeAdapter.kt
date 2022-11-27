6241c61894c42a6c9dc7b9b86be19f1e546e011d
SunWoong <sunwoong1122@gmail.com>
2022-11-27 21:42:17 +0900

    refactor: ItemDiffCallback 공통 클래스로 분리 & 적용

--- app/src/main/java/org/sopt/sample/presentation/home/adapter/HomeAdapter.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/adapter/HomeAdapter.kt
@@ -2,13 +2,16 @@ package org.sopt.sample.presentation.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.sample.data.model.Profile
 import org.sopt.sample.databinding.ItemProfileBinding
+import org.sopt.sample.util.ItemDiffCallback
 
-class HomeAdapter : ListAdapter<Profile, HomeAdapter.ProfileViewHolder>(itemDiffCallBack) {
+class HomeAdapter : ListAdapter<Profile, HomeAdapter.ProfileViewHolder>(ItemDiffCallback<Profile>(
+    onItemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+    onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+)) {
     private lateinit var inflater: LayoutInflater
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
@@ -27,16 +30,6 @@ class HomeAdapter : ListAdapter<Profile, HomeAdapter.ProfileViewHolder>(itemDiff
         fun bind(profile: Profile) {
             binding.profile = profile
             binding.executePendingBindings()
-        }
-    }
-
-    companion object {
-        private val itemDiffCallBack = object : DiffUtil.ItemCallback<Profile>() {
-            override fun areItemsTheSame(oldItem: Profile, newItem: Profile): Boolean =
-                oldItem.id == newItem.id
-
-            override fun areContentsTheSame(oldItem: Profile, newItem: Profile): Boolean =
-                oldItem == newItem
         }
     }
 }
