9a0e885d835ea107b42d738e55243668fa7ba67b
SunWoong <sunwoong1122@gmail.com>
2022-12-02 17:31:30 +0900

    refactor: data source 추상화 제거 & repository 추상화에 따른 변경 사항 적용

--- app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
+++ app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.ViewModelProvider
 import org.sopt.sample.data.api.ApiClient
 import org.sopt.sample.data.api.AuthService
 import org.sopt.sample.data.api.RegresService
-import org.sopt.sample.data.repository.AuthRepository
-import org.sopt.sample.data.repository.RegresRepository
-import org.sopt.sample.data.source.AuthDataSourceImpl
-import org.sopt.sample.data.source.RegresDataSourceImpl
+import org.sopt.sample.data.repository.AuthRepositoryImpl
+import org.sopt.sample.data.repository.RegresRepositoryImpl
+import org.sopt.sample.data.source.remote.AuthDataSource
+import org.sopt.sample.data.source.remote.RegresDataSource
 import org.sopt.sample.presentation.home.viewmodel.HomeViewModel
 import org.sopt.sample.presentation.signin.viewmodel.SignInViewModel
 import org.sopt.sample.presentation.signup.viewmodel.SignUpViewModel
@@ -18,22 +18,22 @@ class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(SignInViewModel::class.java) -> SignInViewModel(
-                AuthRepository(
-                    AuthDataSourceImpl(
+                AuthRepositoryImpl(
+                    AuthDataSource(
                         ApiClient.soptRetrofit.create(AuthService::class.java)
                     )
                 )
             ) as T
             modelClass.isAssignableFrom(SignUpViewModel::class.java) -> SignUpViewModel(
-                AuthRepository(
-                    AuthDataSourceImpl(
+                AuthRepositoryImpl(
+                    AuthDataSource(
                         ApiClient.soptRetrofit.create(AuthService::class.java)
                     )
                 )
             ) as T
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> HomeViewModel(
-                RegresRepository(
-                    RegresDataSourceImpl(
+                RegresRepositoryImpl(
+                    RegresDataSource(
                         ApiClient.regresRetrofit.create(RegresService::class.java)
                     )
                 )
