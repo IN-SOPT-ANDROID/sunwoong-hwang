db6fdf01a74446c156ff88add694cc1a4a546559
SunWoong <sunwoong1122@gmail.com>
2022-12-11 00:47:05 +0900

    feat: 음악 리스트 조회 API 구현

--- app/src/main/java/org/sopt/sample/data/api/MusicService.kt
+++ app/src/main/java/org/sopt/sample/data/api/MusicService.kt
@@ -2,7 +2,9 @@ package org.sopt.sample.data.api
 
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import org.sopt.sample.data.model.MusicListResponse
 import org.sopt.sample.data.model.MusicResponse
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -17,4 +19,10 @@ interface MusicService {
         @Part image: MultipartBody.Part,
         @Part("request") contents: RequestBody
     ): MusicResponse
+
+    /**
+     * 음악 리스트 조회 API
+     */
+    @GET("music/list")
+    suspend fun getMusicList(): MusicListResponse
 }
