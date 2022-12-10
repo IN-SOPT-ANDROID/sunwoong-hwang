c238c36f94e2fbedb7ef99c800ec694eb68a3af3
SunWoong <sunwoong1122@gmail.com>
2022-12-11 00:48:36 +0900

    feat: music view model 의존성 주입

--- app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
+++ app/src/main/java/org/sopt/sample/presentation/common/ViewModelFactory.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.sopt.sample.data.api.ApiClient
 import org.sopt.sample.data.api.AuthService
+import org.sopt.sample.data.api.MusicService
 import org.sopt.sample.data.api.RegresService
 import org.sopt.sample.data.repository.AuthRepositoryImpl
+import org.sopt.sample.data.repository.MusicRepositoryImpl
 import org.sopt.sample.data.repository.RegresRepositoryImpl
 import org.sopt.sample.data.source.remote.AuthDataSource
+import org.sopt.sample.data.source.remote.MusicDataSource
 import org.sopt.sample.data.source.remote.RegresDataSource
 import org.sopt.sample.presentation.home.viewmodel.HomeViewModel
+import org.sopt.sample.presentation.music.viewmodel.MusicViewModel
 import org.sopt.sample.presentation.signin.viewmodel.SignInViewModel
 import org.sopt.sample.presentation.signup.viewmodel.SignUpViewModel
 
@@ -35,6 +39,13 @@ class ViewModelFactory : ViewModelProvider.Factory {
                 RegresRepositoryImpl(
                     RegresDataSource(
                         ApiClient.regresRetrofit.create(RegresService::class.java)
+                    )
+                )
+            ) as T
+            modelClass.isAssignableFrom(MusicViewModel::class.java) -> MusicViewModel(
+                MusicRepositoryImpl(
+                    MusicDataSource(
+                        ApiClient.musicRetrofit.create(MusicService::class.java)
                     )
                 )
             ) as T
